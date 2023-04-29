d45b7f12ea9b2a5aa87486c998a67681e499735a
victordev13 <vtrcarvalho.13@gmail.com>
2023-04-29 12:24:21 -0300

    feat: MultiStep component

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,35 @@
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+const MAX_STEPS = 32
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  if (size > MAX_STEPS) {
+    size = MAX_STEPS
+  }
+
+  if (currentStep > size) {
+    currentStep = size
+  }
+
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}{' '}
+      </Label>
+      <Steps
+        css={{
+          '--steps-size': size,
+        }}
+      >
+        {Array.from({ length: size }, (_, i) => ++i).map((step) => (
+          <Step key={step} active={currentStep >= step} />
+        ))}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
