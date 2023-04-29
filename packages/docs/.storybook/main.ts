7de0838935a3b63f48bc4de0a64870d48c240ba8
victordev13 <vtrcarvalho.13@gmail.com>
2023-04-28 22:44:07 -0300

    feat: update storybook

---
+++ packages/docs/.storybook/main.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../src/pages/**/*.stories.mdx', '../src/stories/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,22 @@
+import { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    backgrounds: {
+      default: 'dark',
+    }
+  },
+};
+
+export default preview;
